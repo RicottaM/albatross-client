c1dc11f4c335df0ac77c5701deb996062652ac96
RicottaM <jaskulskim.gdynia@gmail.com>
2025-05-30 04:04:51 +0200

    Added not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '@/views/Login/Login';
 import Home from '@/views/Home/Home';
+import NotFound from './views/NotFound/NotFound';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ src/views/NotFound/NotFound.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import './NotFound.css';
+import { RiArrowGoBackFill } from 'react-icons/ri';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="notfound-wrapper">
+      <img src="/404.png" alt="404 Not Found" width={'20%'} className="notfound-image" />
+      <div className="notfound-content">
+        <p>Page not found.</p>
+        <img src="/curved-arrow.svg" className="home-icon" width={64} onClick={() => navigate('/home')} />
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
